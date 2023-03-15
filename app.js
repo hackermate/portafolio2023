60de3003c88e12b79545e37ee60479e164dc9c45
hackermat <villacresa@gmail.com>
2023-03-15 10:24:38 -0500

    init: protafolio

---
+++ app.js
@@ -0,0 +1,34 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controlls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function PageTransition(){
+  //Buton click active class
+  for(let i = 0; i < sectBtn.length; i++){
+    sectBtn[i].addEventListener('click', function(){
+      let currentBtn = document.querySelectorAll('.active-btn');
+      currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+      this.className += ' active-btn';
+    })
+  }
+  allSections.addEventListener('click', (e) => {
+   const id = e.target.dataset.id; 
+   if(id){
+    //remueve la seleccion de los botones
+    sectBtns.forEach((btn)=> {
+      btn.classList.remove('active')
+    })
+      e.target.classList.add('active')
+     //activa otra sección
+     sections.forEach((section)=> {
+      section.classList.remove('active')
+     })
+
+     const element = document.getElementById(id);
+     element.classList.add('active');
+   }
+  })
+}
+
+PageTransition();
