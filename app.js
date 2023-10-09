540a38bcb2fd339c22b27016c012f5eca4ed8807
hackermat <villacresa@gmail.com>
2023-10-09 14:56:29 -0500

    update

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
-const sections = document.querySelectorAll(".section");
-const sectBtns = document.querySelectorAll(".controlls");
-const sectBtn = document.querySelectorAll(".control");
-const allSections = document.querySelector(".main-content");
-
-function PageTransition() {
-  //Buton click active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener("click", function () {
-      let currentBtn = document.querySelectorAll(".active-btn");
-      currentBtn[0].className = currentBtn[0].className.replace(
-        "active-btn",
-        ""
-      );
-      this.className += " active-btn";
+(function () {
+    [...document.querySelectorAll(".control")].forEach(button => {
+        button.addEventListener("click", function() {
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
+        })
     });
-  }
-  allSections.addEventListener("click", (e) => {
-    const id = e.target.dataset.id;
-    if (id) {
-      //remueve la seleccion de los botones
-      sectBtns.forEach((btn) => {
-        btn.classList.remove("active");
-      });
-      e.target.classList.add("active");
-      //activa otra sección
-      sections.forEach((section) => {
-        section.classList.remove("active");
-      });
-      const element = document.getElementById(id);
-      element.classList.add("active");
-    }
-  });
-}
-
-PageTransition();
+    document.querySelector(".theme-btn").addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+    })
+})();
